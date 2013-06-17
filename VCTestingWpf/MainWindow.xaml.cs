71523d17147f5baa94d371d9f1e9dd344e1b850a
Stuart <lodge.stuart@gmail.com>
2013-06-17 11:57:57 +0100

    Wpf and WinRt tests

---
+++ VCTestingWpf/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace VCTestingWpf
+{
+    public class TestDataContext
+    {
+        private List<int> _values;
+        public List<int> Values
+        {
+            get { return _values; }
+            set { _values = value; }
+        }
+    }
+
+    public class MyValueConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null)
+                return "Converter:" + "NULL";
+
+            return "Converter:" + value.ToString();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = new TestDataContext();
+        }
+    }
+}
